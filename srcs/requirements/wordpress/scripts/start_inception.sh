671b74f703f61125a7c8a21f1fdc5dda7726827d
Simon Cros <scros@student.42lyon.fr>
2021-11-22 12:34:01 +0100

    Patch

--- srcs/requirements/wordpress/scripts/start_inception.sh
+++ srcs/requirements/wordpress/scripts/start_inception.sh
@@ -1,5 +1,3 @@
-set -e
-
 if ! wp --allow-root core is-installed; then
     wp --allow-root core install --url="$WP_URL" --title="$WP_TITLE" --admin_user="$WP_USER" --admin_email="$WP_EMAIL" --admin_password="$WP_PASSWORD"
 	wp --allow-root user create test test@example.com
