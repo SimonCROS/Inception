92eea61574b486a870cc7e75353e63084dfa1be4
Simon Cros <scros@student.42lyon.fr>
2021-11-22 12:11:09 +0100

    Add security in script

--- srcs/requirements/wordpress/scripts/start_inception.sh
+++ srcs/requirements/wordpress/scripts/start_inception.sh
@@ -1,3 +1,5 @@
+set -e
+
 if ! wp --allow-root core is-installed; then
     wp --allow-root core install --url="$WP_URL" --title="$WP_TITLE" --admin_user="$WP_USER" --admin_email="$WP_EMAIL" --admin_password="$WP_PASSWORD"
 	wp --allow-root user create test test@example.com
